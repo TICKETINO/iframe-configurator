09d9b5e325b637866c75c2aa056e67fda51a7e38
Benjamin Schneider <mail@benjaminschneider.ch>
2017-05-30 20:04:38 +0200

    Add querystring parameters to allow sharing of configuration

--- js/app.js
+++ js/app.js
@@ -7,6 +7,13 @@ $(".header").click(function () {
     });
 });
 
+// Read querystring parameters
+function qs(key) {
+    key = key.replace(/[*+?^$.\[\]{}()|\\\/]/g, "\\$&"); // escape RegEx meta chars
+    var match = location.search.match(new RegExp("[?&]"+key+"=([^&]+)(&|$)"));
+    return match && decodeURIComponent(match[1].replace(/\+/g, " "));
+}
+
 // Select iframe code on click
 function selectText(containerid) {
     if (document.selection) {
@@ -28,19 +35,29 @@ function ViewModel() {
 	self.baseURL				= ko.observable("https://v3.ticketino.com/v3/");
 	self.embedURL				= ko.observable("/Event/Embed/");
 
-	self.languageCode 			= ko.observable("de");
-	self.eventId 				= ko.observable("12345");
-    self.color 					= ko.observable("").extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
-    self.backgroundColor 		= ko.observable("").extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
-    self.buttonBackColor 		= ko.observable("").extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
-    self.buttonForeColor 		= ko.observable("").extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
-    self.promoBoxColor 			= ko.observable("").extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
-    self.buttonHoverForeColor 	= ko.observable("").extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
-    self.buttonHoverBackColor 	= ko.observable("").extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
-    self.dropdownBackColor 		= ko.observable("").extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
-    self.dropdownForeColor 		= ko.observable("").extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
-    self.width                  = ko.observable(700).extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
-    self.height                 = ko.observable(450).extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
+	self.languageCode 			= ko.observable(qs('languageCode') || "de");
+	self.eventId 				= ko.observable(qs('eventId') || "12345");
+    self.width                  = ko.observable(qs('width') || 700);
+    self.height                 = ko.observable(qs('height') || 450);
+
+    self.color 					= ko.observable(qs('color') || "").
+                                    extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
+    self.backgroundColor 		= ko.observable(qs('backgroundColor') || "").
+                                    extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
+    self.buttonBackColor 		= ko.observable(qs('buttonBackColor') || "").
+                                    extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
+    self.buttonForeColor 		= ko.observable(qs('buttonForeColor') || "").
+                                    extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
+    self.promoBoxColor 			= ko.observable(qs('promoBoxColor') || "").
+                                    extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
+    self.buttonHoverForeColor 	= ko.observable(qs('buttonHoverForeColor') || "").
+                                    extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
+    self.buttonHoverBackColor 	= ko.observable(qs('buttonHoverBackColor') || "").
+                                    extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
+    self.dropdownBackColor 		= ko.observable(qs('dropdownBackColor') || "").
+                                    extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
+    self.dropdownForeColor 		= ko.observable(qs('dropdownForeColor') || "").
+                                    extend({ rateLimit: { timeout: 500, method: "notifyWhenChangesStop" } });
 
     self.iframeSrc = ko.computed(function() {
     	return 	self.baseURL() +
@@ -61,6 +78,27 @@ function ViewModel() {
 
     self.iframeCode = ko.computed(function() {
     	return	'<iframe src="' + self.iframeSrc() + '" width="' + self.width() + '" height="' + self.height() + '"></iframe>';
+    });
+
+    self.shareConfigUrl = ko.computed(function() {
+        var cfgUrl = 'https://ticketino.github.io/iframe-configurator/?'
+        if (self.languageCode() != '') { cfgUrl += 'languageCode=' + self.languageCode() + '&' }
+        if (self.eventId() != '') { cfgUrl += 'eventId=' + self.eventId() + '&' }
+        if (self.width() != '') { cfgUrl += 'width=' + self.width() + '&' }
+        if (self.height() != '') { cfgUrl += 'height=' + self.height() + '&' }
+        if (self.color() != '') { cfgUrl += 'color=' + self.color().replace("#", "") + '&' }
+        if (self.backgroundColor() != '') { cfgUrl += 'backgroundColor=' + self.backgroundColor().replace("#", "") + '&' }
+        if (self.buttonBackColor() != '') { cfgUrl += 'buttonBackColor=' + self.buttonBackColor().replace("#", "") + '&' }
+        if (self.buttonForeColor() != '') { cfgUrl += 'buttonForeColor=' + self.buttonForeColor().replace("#", "") + '&' }
+        if (self.promoBoxColor() != '') { cfgUrl += 'promoBoxColor=' + self.promoBoxColor().replace("#", "") + '&' }
+        if (self.buttonHoverForeColor() != '') { cfgUrl += 'buttonHoverForeColor=' + self.buttonHoverForeColor().replace("#", "") + '&' }
+        if (self.buttonHoverBackColor() != '') { cfgUrl += 'buttonHoverBackColor=' + self.buttonHoverBackColor().replace("#", "") + '&' }
+        if (self.dropdownBackColor() != '') { cfgUrl += 'dropdownBackColor=' + self.dropdownBackColor().replace("#", "") + '&' }
+        if (self.dropdownForeColor() != '') { cfgUrl += 'dropdownForeColor=' + self.dropdownForeColor().replace("#", "") + '&' }
+
+        if (cfgUrl.slice(-1) === '&') { cfgUrl = cfgUrl.slice(0, -1); }
+
+        return cfgUrl;
     });
 }
 
@@ -83,5 +121,5 @@ $(document).ready(function() {
     $('#dropdownBackColor').colorpicker(opts);
     $('#dropdownForeColor').colorpicker(opts);
 
-    $('#iframe-code-output').click(selectText);
+    $('.output').click(selectText);
 });
